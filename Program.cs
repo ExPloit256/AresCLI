aad551fea48674c45141a587e879148269301f0b
Explo!t <bigboy.thebitcoin@gmail.com>
2021-12-06 11:49:58 +0100

    Added Test Title

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace AresCLI
     {
         static void Main(string[] args)
         {
-            string docName = "", docPath = "";
+            string docName = "", docPath = "", testTitle = "";
             int docNumber = 0;
 
             PdfDocument stubDoc = new PdfDocument();
@@ -30,7 +30,7 @@ namespace AresCLI
             do
             {
                 ConsoleDrawInit();
-                DocInit(docName, docPath, docNumber, render, stubDoc, stubPage);
+                DocInit(docName, docPath, testTitle, docNumber, render, stubDoc, stubPage);
                 Console.ReadLine();
             } while (true);
 
@@ -46,19 +46,21 @@ namespace AresCLI
         static void DocInit
             (string docName,
              string docPath,
+             string testTitle,
              int docNumber,
              XGraphics render,
              PdfDocument stubDoc,
              PdfPage stubPage)
         {
-            DocNameCheck:
-            Console.WriteLine("Inserisci il titolo del File: ");
+        #region DocName&DefInfoinit
+        DocNameCheck:
+            Console.WriteLine("Inserisci il nome del File: ");
             docName = Console.ReadLine() + ".pdf";
 
             if (string.IsNullOrWhiteSpace(docName))
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Il titolo non può essere nullo! \n(premi un qualsiasi tasto per continuare)\n");
+                Console.WriteLine("Il nome del file non può essere nullo! \n(premi un qualsiasi tasto per continuare)\n");
                 Console.ReadKey();
 
                 Console.ForegroundColor = ConsoleColor.White;
@@ -72,9 +74,10 @@ namespace AresCLI
             new XRect(221, 55, stubPage.Width, stubPage.Height), XStringFormat.TopLeft);
             render.DrawString("CLASSE: _________", Fonts.NDC, XBrushes.Black,
             new XRect(412, 55, stubPage.Width, stubPage.Height), XStringFormat.TopLeft);
+        #endregion
 
-            
-            DocNumCheck:
+        #region DocNum
+        DocNumCheck:
             Console.WriteLine("\nInserisci il numero della verifica: ");
             if(!int.TryParse(Console.ReadLine(), out docNumber))
             {
@@ -86,9 +89,39 @@ namespace AresCLI
                 goto DocNumCheck;
             }
 
-            render.DrawString($"Verifica Numero {docNumber}", Fonts.SubTitle, XBrushes.Black,
-            new XRect(15, 15, stubPage.Width, stubPage.Height), XStringFormat.TopLeft);
+            render.DrawString($"Verifica Numero {docNumber}", Fonts.SmallTitle, XBrushes.Black,
+            new XRect(10, 10, stubPage.Width, stubPage.Height), XStringFormat.TopLeft);
+        #endregion
 
+        #region TestTitle
+        TestTitle:
+            Console.WriteLine("\nInserisci il titiolo della verifica: ");
+            testTitle =  Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(testTitle))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Il titolo della verifica non può essere nullo! \n(premi un qualsiasi tasto per continuare)\n");
+                Console.ReadKey();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                goto TestTitle;
+            }
+            else
+            {
+                if (testTitle.Length > 35)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Il titolo della verifica non può essere più lungo di 35 caratteri! \n(premi un qualsiasi tasto per continuare)\n");
+                    Console.ReadKey();
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    goto TestTitle;
+                }
+            }
+            render.DrawString($"{testTitle}", Fonts.Title, XBrushes.Black,
+            new XRect(0, 25, stubPage.Width, stubPage.Height), XStringFormat.TopCenter);
+            Console.WriteLine(testTitle.Length);
+            #endregion
             stubDoc.Save(docPath);
         }
     }
@@ -97,6 +130,7 @@ namespace AresCLI
     {
         public static XFont Title = new XFont("Verdana", 20, XFontStyle.Bold);
         public static XFont SubTitle = new XFont("Verdana", 13, XFontStyle.Bold);
+        public static XFont SmallTitle = new XFont("Verdana", 8, XFontStyle.Bold);
         public static XFont NDC = new XFont("Verdana", 15, XFontStyle.Regular);
     }
 }
